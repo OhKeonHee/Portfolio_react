aab003f587e5dbfc40cbe0a38936016ee8a20387
OhKeonHee <98ogh@naver.com>
2024-04-02 19:56:59 +0900

    .

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useState } from 'react'
 import { styled } from 'styled-components'
+import { motion } from 'framer-motion'
 import ReactIcon from '../icons/ReactIcon'
 import HtmlIcon from '../icons/HtmlIcon'
 import CssIcon from '../icons/CssIcon'
 import JsIcon from '../icons/JsIcon'
+import TsIcon from '../icons/TypeScript'
 import mealkit_mobile from '../imgs/mealkit_mobile.PNG'
 import academy_uiux_page from '../imgs/academy_uiux_page.PNG'
 import music_play from '../imgs/music_play.PNG'
@@ -20,9 +22,114 @@ import teamproject from '../imgs/teamproject.PNG'
 import react_portfolio from '../imgs/react_portfolio.PNG'
 import sollink from '../imgs/sollink.JPG'
 import { VscGithubInverted, VscGlobe } from 'react-icons/vsc'
-import { Icons, Overlay, Project, Project_num, Project_title, SkillSec, Wrapper } from './styles'
 
-
+const Wrapper = styled.div`
+  width: calc(100vw - 300px);
+  color: var(--font-color);
+  padding: 10px 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  gap: 8px;
+`
+const Title = styled.h1`
+  font-family: Consolas, 'Courier New', monospace;
+  padding: 10px 20px;
+  color: var(--font-color);
+`
+const Project = styled.div`
+  width: 32.7%;
+  /* border: 1px solid red; */
+  background-color: var(--explorer-bg-color);
+  position: relative;
+  margin-bottom: 20px;
+`
+const Project_num = styled.div`
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  background-color: var(--footer-btn);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  font-weight: bold;
+  color: white;
+  top: 5px;
+  left: 5px;
+`
+const SkillSec = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px;
+`
+const Icons = styled.div`
+  display: flex;
+  gap: 5px;
+  svg {
+    width: 30px;
+  }
+`
+const Img = styled(motion.img)`
+  width: 100%;
+`
+const Overlay = styled(motion.div)`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  opacity: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  gap: 10px;
+  white-space: pre-line;
+  > h4 {
+    color: lightgray;
+    letter-spacing: 1px;
+    line-height: 2;
+    z-index: 9;
+    padding: 0 10px;
+    overflow-y: scroll;
+    padding: 5px;
+  }
+  > div {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 100%;
+    > a {
+      color: #fff;
+      width: 50%;
+      margin: 0 3px;
+      text-align: center;
+      display: block;
+      background-color: var(--footer-btn);
+      border: 3px solid var(--footer-btn);
+      padding: 7px;
+      font-weight: bold;
+      font-size: 14px;
+      &:hover {
+        background-color: transparent;
+        border: 3px solid var(--footer-btn);
+      }
+    }
+  }
+  
+`
+const Project_title = styled.h3`
+  text-align: center;
+  padding: 10px 0;
+  border-bottom: 1px solid var(--font-color);
+`
+const Project_read = styled.h3`
+  font-weight: 300;
+  padding: 10px 5px;
+  line-height: 1.8;
+  white-space: pre-line;
+`
 const project_arr = [
   {
     id: 14,
@@ -282,7 +389,6 @@ const Projects = () => {
                 <it.HtmlIcon />
                 <it.CssIcon />
                 <it.JsIcon />
-                <it.TsIcon />
                 <it.ReactIcon />
               </Icons>
               {it.date}
